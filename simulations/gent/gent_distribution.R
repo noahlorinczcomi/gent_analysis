5d4767b2b4d75f75e20ba30c44136c9a32876f60
Noah Lorincz-Comi <39975632+noahlorinczcomi@users.noreply.github.com>
2024-10-09 12:54:42 -0400

    Add files via upload

--- simulations/gent/gent_distribution.R
+++ simulations/gent/gent_distribution.R
@@ -1,5 +1,6 @@
 rm(list=ls(all=TRUE))
 library(mvnfast);library(ggplot2);library(ggh4x)
+source('simulations/gent/functions.R')
 ##################################################################################
 # changing ms, ngwas, LD density
 ms=c(50,100,150)
@@ -8,6 +9,7 @@ rhos=c(0.1,0.5,0.9)
 h2=0
 nref=500
 niter=10000
+#m.=n.=r.=1
 for(m. in 1:length(ms)) {
   for(n. in 1:length(ngwas)) {
     for(r. in 1:length(rhos)) {
@@ -21,7 +23,7 @@ for(m. in 1:length(ms)) {
       s2hat=rchisq(prod(dim(betahat)),ngwas[n.]-1)/(ngwas[n.]-1)/ngwas[n.]
       zhat=betahat/sqrt(s2hat)
       stats=rowSums(zhat^2)
-      # estimate null distribution
+      # estimate null distribution using approximation
       R=rWishart(niter,nref,R0) # LD matrix estimates from reference panel
       R=lapply(1:niter,function(h) cov2cor(R[,,h]))
       trRRs=lapply(1:niter,function(h) 2*tr(R[[h]]%*%R[[h]]))
@@ -31,8 +33,12 @@ for(m. in 1:length(ms)) {
       # plot
       main=paste0('# SNPs=',ms[m.],'; # ppl.=',ngwas[n.]/1e3,'K; LD rho=',rhos[r.])
       linehist(stats,linecol='black',breaks=30,main=main,xlab='simulated statistics')
+      dd=density(stats)
+      q0=qgamma(0.95,shape=alpha,rate=xi)
+      lab.=paste0('Type I error: ',round(sum(stats>q0)/niter*100,2),'%')
+      text(x=median(stats),median(dd$y)*0.2,label=lab.,cex=4/5)
       curve(dgamma(x,shape=alpha,rate=xi),min(stats),max(stats),add=T,col='blue')
-      legend('topright',legend=c('empirical density','theoretical density'),lty=c(1,1),col=c('black','blue'),cex=1/2)
+      legend('topright',legend=c('empirical density','theoretical density'),lty=c(1,1),col=c('black','blue'),cex=2/3.5)
     }
   }
 }
